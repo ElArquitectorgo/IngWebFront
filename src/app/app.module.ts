e3e77c849f245c62ce63b937ccff547959ddd96a
ElArquitectorgo <mega2814s@gmail.com>
2023-12-12 20:35:38 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,26 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 
+import {MatTableModule} from '@angular/material/table'; 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSnackBarModule, MatSnackBar} from '@angular/material/snack-bar';
+import { HomeComponent } from './home/home.component'; 
+
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
