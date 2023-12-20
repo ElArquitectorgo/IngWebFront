1fa849d3322017e9e03ccc0eb1024499522c494a
susanfg <susanafernandez@uma.es>
2023-12-20 12:29:38 +0100

    informe-create component

---
+++ src/app/informe-create/informe-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-informe-create',
+  templateUrl: './informe-create.component.html',
+  styleUrl: './informe-create.component.css'
+})
+export class InformeCreateComponent {
+
+}
